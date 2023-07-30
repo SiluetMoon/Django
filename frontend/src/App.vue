2b4e74625729e2ec315dd4bc079a51e47dbabe8c
SiluetMoon <artembalayan03@gmail.com>
2023-07-30 13:52:18 +0300

    SiluetMoon

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink,RouterView } from 'vue-router';
+
+</script>
+<template>
+  <div>
+    <RouterView/>
+    <h1 class="text-cyan-50">
+        Hello world!
+    </h1>
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="text">
+    asd
+  </div>
+</template>
+
+<style>
+
+</style>
